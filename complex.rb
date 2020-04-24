c6771090acef4ee647cf9db1e64a6d0f084d2af2
Heba10 <hebe@yahoo.com>
2020-04-24 21:14:00 +0200

    the add bulk function to add the array num

--- complex.rb
+++ complex.rb
@@ -24,16 +24,35 @@ class ComplexNum
 
 		return ComplexNum.new(real, imag)
 	end
+    #////////////////////////////////////////////////////////////////////////////////////////////////
+    # bulk_add
+    def self.bulk_add(nums)
+		complex_num = nums[0]
+		i = 1
+        puts "steps to adding array complex number\n"
+			while i < nums.count
+				complex_num += nums[i]
+				i += 1
+			end 		
 
+			puts "the add bulk ===>>> (#{complex_num.real} + #{complex_num.imag}i)"
+
+			return complex_num
+	end
 
 
 end
 
 complex1 = ComplexNum.new(3, 3)
 complex2 = ComplexNum.new(8, 8)
+complex3 = ComplexNum.new(3, 3)
+
+
+complex_array = [complex1, complex2, complex3]
 
 add = complex1 + complex2
 mult = complex1 * complex2
+ComplexNum.bulk_add(complex_array)
 
 
 
