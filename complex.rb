c993a6561883ccd16ef33c6bb4ce8de177e6811c
Heba10 <hebe@yahoo.com>
2020-04-24 22:08:45 +0200

    get_stats​ function to calculate how many times the calculator used categorized by the operations types

--- complex.rb
+++ complex.rb
@@ -1,4 +1,7 @@
 class ComplexNum
+    # Class Variables
+    @@countAdd = 0
+	@@countMult = 0
     # getter method
     attr_reader :real, :imag
     # init the variable
@@ -8,19 +11,25 @@ class ComplexNum
     end
     # add function
     def +(num)
+        @@countAdd+= 1
 		real = self.real + num.real
 		imag = self.imag + num.imag
 		
-		puts "(#{self.real} + #{self.imag}i) + (#{num.real} + #{num.imag}i) = #{real} + #{imag}i"
+        puts "(#{self.real} + #{self.imag}i) + (#{num.real} + #{num.imag}i) = #{real} + #{imag}i \n"
+        puts "************************************************************************************"
+        
 		return ComplexNum.new(real, imag)
     end
     # ///////////////////////////////////////////////////////////////////////////////////////////////
     # mult function
     def *(num)
+        @@countMult += 1
 		real = (self.real * num.real) - (self.imag * num.imag)
-		imag = (self.real * num.imag) + (self.imag * num.real)
+        imag = (self.real * num.imag) + (self.imag * num.real)
+   
 	
-		puts "(#{self.real} + #{self.imag}i) * (#{num.real} + #{num.imag}i) = #{real} + #{imag}i"		
+        puts "(#{self.real} + #{self.imag}i) * (#{num.real} + #{num.imag}i) = #{real} + #{imag}i \n"
+     	
 
 		return ComplexNum.new(real, imag)
 	end
@@ -28,8 +37,9 @@ class ComplexNum
     # bulk_add
     def self.bulk_add(nums)
 		complex_num = nums[0]
-		i = 1
-        puts "steps of add array complex number\n\n"
+        i = 1
+        puts "**************************************bulk_add**********************************************"
+        puts "steps of add array complex number\n"
        
 			while i < nums.count
 				complex_num += nums[i]
@@ -47,8 +57,8 @@ class ComplexNum
         complex_num = nums[0]
 
                         i = 1
-            
-            puts "steps of Multiply array complex number\n\n"
+            puts "***************************************bulk_multiply*********************************************"
+            puts "steps of Multiply array complex number\n"
                         while i < nums.count
                             complex_num *= nums[i]
                                 i += 1
@@ -59,6 +69,12 @@ class ComplexNum
                         return complex_num
                
 
+    end
+    
+    def get_stats
+        puts "*************************************get_stats***********************************************"
+        puts " count of multiply  #{@@countMult} "
+        puts " count of add  #{@@countAdd}"
 	end
 
 
@@ -74,11 +90,13 @@ complex3 = ComplexNum.new(3, 3)
 
 complex_array = [complex1, complex2, complex3]
 
-add = complex1 + complex2
+add1 = complex1 + complex2
+
 mult = complex1 * complex2
+
 ComplexNum.bulk_add(complex_array)
 ComplexNum.bulk_multiply(complex_array)
-
+complex1.get_stats
 
 
 
